bcd813330a68052e16c970963c90a5cb4421dcbb
Nayan Gautam <gtmnayan@gmail.com>
2024-01-17 17:59:44 +0545

    add notes

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,6 +1,8 @@
 import { TSConfckCache, parse } from 'tsconfck';
 
 const cache = new TSConfckCache();
+
+// reading the value immediately after one resolves gives inconsistent results
 (async () => {
 	console.log('1-start');
 	console.log(await parse('src/routes/foo.ts', { cache, ignoreNodeModules: true }));
@@ -11,3 +13,11 @@ const cache = new TSConfckCache();
 	console.log(await parse('src/routes/bar.ts', { cache, ignoreNodeModules: true }));
 	console.log('2-end');
 })();
+
+
+// after waiting for both to resolve gives consistent results
+// const [foo, bar] = await Promise.all([
+//     parse('src/routes/foo.ts', { cache, ignoreNodeModules: true }),
+//     parse('src/routes/bar.ts', { cache, ignoreNodeModules: true })
+// ]);
+// console.log(foo.tsconfig, bar.tsconfig);
